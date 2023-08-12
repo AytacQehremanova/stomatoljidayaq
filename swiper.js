745a010c4c3ab99901ed7229f13064f677e67e10
aytac <qehremanova.aytac2003@gmail.com>
2023-08-13 01:21:02 +0400

    form,partners,workers sectionlari elave edildi

--- swiper.js
+++ swiper.js
@@ -14,3 +14,57 @@ const bannerSwiper = new Swiper(".banner-swiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+
+  const partnersSwiper = new Swiper(".partners-swiper", {
+    slidesPerView: 1,
+    
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints:{
+      700:{
+        slidesPerView:3,
+        spaceBetween:60,
+      },
+
+      1024:{
+        slidesPerView:5,
+        spaceBetween:50,
+      }
+    }
+  });
+
+
+
+  const workerSwiper = new Swiper(".workers-swiper", {
+    slidesPerView: 1,
+    
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints:{
+      700:{
+        slidesPerView:3,
+        spaceBetween:60,
+      },
+
+      1024:{
+        slidesPerView:6,
+        spaceBetween:50,
+      }
+    }
+  });
+
+  
